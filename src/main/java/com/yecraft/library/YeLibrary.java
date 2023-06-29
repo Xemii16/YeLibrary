1bbada6a183905869a2c1b5d899166e63e87860e
Xemii16 <balamutmaks@gmail.com>
2023-06-29 22:49:57 +0300

    Initial commit

---
+++ src/main/java/com/yecraft/library/YeLibrary.java
@@ -0,0 +1,17 @@
+package com.yecraft.library;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YeLibrary extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
